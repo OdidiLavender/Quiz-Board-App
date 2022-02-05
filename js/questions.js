d8d7de0d0f4941d52baec9d8c36cb60f7951f5ea
OdidiLavender <odidilavender@gmail.com>
2022-02-05 16:15:23 +0300

    on the js question created an array of questions, anwers and choices

---
+++ js/questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: " To create HTML page, you need?",
+    answer: "Both [A] and [B]",
+    options: [
+      "Web browser",
+      "text editor",
+      "Both [A] and [B]",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "a are the tags used for?",
+    answer: "Adding links to your page",
+    options: [
+        "Adding image",
+        "Aligning text",
+        "Audio-voiced text",
+        "Adding links to your page",
+    ]
+  },
+    {
+    numb: 3,
+    question: "The BODY tag is usually used after?",
+    answer: "HEAD tag",
+    options: [
+      "HTML tag",
+      "EM tag",
+      "TITLE tag",
+      "HEAD tag"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does the br tag add to your webpage?",
+    answer: " Line break",
+    options: [
+      "Long break",
+      "Paragraph break",
+      " Line break",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which type of language is Javascript ?",
+    answer: " Line break",
+    options: [
+    " Programming",
+     "Scripting",
+    "Markup",
+    "None of the above"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which is not valid data type in Javascript ?",
+    answer: "  float",
+    options: [
+      "Undefinded",
+      "Boolean",
+      "float",
+      "Number",
+    ]
+  },
+  {
+    numb: 8,
+    question: "Gif and jpg are the two main types of what?",
+    answer: "images",
+    options: [
+      "videos",
+      "Paragraph break",
+      " animated effects",
+      "images"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Increasing the cellpadding means ?",
+    answer: "Increase the distance between cell and content",
+    options: [
+      "Increase the softness of your site",
+      "Increase the space between cells",
+      "Increase the distance between cell and content",
+      "All of the above",
+    ]
+  },
+  {
+    numb: 10,
+    question: "Adding a border to your image helps the visitor to recognize it as what?",
+    answer: " A link",
+    options: [
+      "A frame",
+      "A link" ,
+      "A picture",
+      " None of the above",
+    ]
+  },
+];
